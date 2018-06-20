21a5a8f21c78bb6f634ad817b8dd8d47ff087cdb
mottledMantis <jamesmatheson35@gmail.com>
2018-06-20 00:50:02 -0400

    Added dummy variables for company and product_category

--- script_for_refine.R
+++ script_for_refine.R
@@ -29,6 +29,65 @@ for (i in 1:nrow(refine_mod)) {
     
 }
 
+#concatenate address columns to full_address, separate with commas
+refine_mod$full_address <- paste(refine_mod$address, ", ", refine_mod$city, ", ", refine_mod$country)
 
 
+#Add four binary (1 or 0) columns for company:
+#company_philips, company_akzo, company_van_houten and company_unilever.
+for (i in 1:nrow(refine_mod)) {
+  if (refine_mod[i, "company"] == "philips") {
+    refine_mod[i, "company_philips"] = 1
+  }
+  else {
+    refine_mod[i, "company_philips"] = 0
+  }
+  if (refine_mod[i, "company"] == "akzo") {
+    refine_mod[i, "company_akzo"] = 1
+  }
+  else {
+    refine_mod[i, "company_akzo"] = 0
+  }
+  if (refine_mod[i, "company"] == "van houten") {
+    refine_mod[i, "company_van_houten"] = 1
+  }
+  else {
+    refine_mod[i, "company_van_houten"] = 0
+  }
+  if (refine_mod[i, "company"] == "unilever") {
+    refine_mod[i, "company_unilever"] = 1
+  }
+  else {
+    refine_mod[i, "company_unilever"] = 0
+  }
+}
 
+
+#Add four binary (1 or 0) columns for product category:
+#product_smartphone, product_tv, product_laptop and product_tablet.
+for (i in 1:nrow(refine_mod)) {
+  if (refine_mod[i, "product_category"] == "Smartphone") {
+    refine_mod[i, "product_smartphone"] = 1
+  }
+  else {
+    refine_mod[i, "product_smartphone"] = 0
+  }
+  if (refine_mod[i, "product_category"] == "TV") {
+    refine_mod[i, "product_tv"] = 1
+  }
+  else {
+    refine_mod[i, "product_tv"] = 0
+  }
+  if (refine_mod[i, "product_category"] == "Laptop") {
+    refine_mod[i, "product_laptop"] = 1
+  }
+  else {
+    refine_mod[i, "product_laptop"] = 0
+  }
+  if (refine_mod[i, "product_category"] == "Tablet") {
+    refine_mod[i, "product_tablet"] = 1
+  }
+  else {
+    refine_mod[i, "product_tablet"] = 0
+  }
+}
